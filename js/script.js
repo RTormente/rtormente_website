fa6ca70dcd4596bd07175388af76a47b97afcf9b
Tormente, Rodrigo <118151332+RTormente@users.noreply.github.com>
2026-08-23 20:24:41 -0300

    teste add automatico

--- js/script.js
+++ js/script.js
@@ -38,21 +38,20 @@ function generateVCard() {
     return lines.filter(Boolean).join("\r\n");
 }
 
-function downloadVCard() {
+function openVCard() {
     const vcard = generateVCard();
 
     const blob = new Blob([vcard], { type: "text/vcard;charset=utf-8" });
-
     const url = URL.createObjectURL(blob);
 
     const link = document.createElement("a");
-    link.href = url;
+    /*link.href = url;
     link.download = `rodrigo-tormente.vcf`;
-
     link.click();
     link.remove();
 
-    URL.revokeObjectURL(url);
+    URL.revokeObjectURL(url);*/
+    window.location.href = url;
 }
 
 /**  Shared  **/
@@ -129,13 +128,13 @@ function createModal(htmlContent) {
 
 document
     .querySelector("#profile__save")
-    .addEventListener("click", () => downloadVCard());
+    .addEventListener("click", () => openVCard());
 
 document.querySelector("#profile__share").addEventListener("click", shared);
 
 document.querySelector("#profile__qr").addEventListener("click", openQR);
 
 if (window.location.hash === "#add") {
-    downloadVCard();
+    openVCard();
     window.history.replaceState(null, null, " ");
 }
